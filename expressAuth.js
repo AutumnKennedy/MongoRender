e14ef7da477bdb7773c90e55f26da62df6388fda
Autumn Kennedy <113925712+AutumnKennedy@users.noreply.github.com>
2024-03-23 03:50:38 +0000

    create and clear cookies

--- expressAuth.js
+++ expressAuth.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { MongoClient } = require("mongodb");
 const { v4: uuidv4 } = require('uuid');
+var cookieParser = require('cookie-parser');
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 const uri = process.env.MONGODB_URI;
@@ -14,6 +15,7 @@ console.log('Server started at http://localhost:' + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes will go here
 
@@ -37,6 +39,9 @@ app.get('/api/mongo/register', function(req, res) {
 
 // Default route:
 app.get('/', function(req, res) {
+    const viewCookies = `<p><a href="/api/mongo/view-cookies">View Active Cookies</a></p>`;
+    const clearCookies = `<p><a href="/api/mongo/clear-cookies">Clear Cookies</a></p>`;
+  
     res.send(`
     <h2>Login</h2>
     <form action="/api/mongo/login" method="post">
@@ -51,13 +56,10 @@ app.get('/', function(req, res) {
       <button type="submit">Login</button>
     </form>
     <p>Don't have an account? <a href="/api/mongo/register">Register</a></p>
+    ${viewCookies}
+    ${clearCookies}
   `);
 });
-
-
-// app.get('/say/:name', function(req, res) {
-//   res.send('Hello ' + req.params.name + '!');
-// });
 
 
 // Route to access database:
@@ -92,6 +94,8 @@ app.post('/api/mongo/register', function(req, res) {
 
 app.post('/api/mongo/login', function(req, res) {
     const { username, password } = req.body;
+    const viewCookies = `<p><a href="/api/mongo/view-cookies">View Active Cookies</a></p>`;
+    const clearCookies = `<p><a href="/api/mongo/clear-cookies">Clear Cookies</a></p>`;
     
     const client = new MongoClient(uri);
   
@@ -107,11 +111,15 @@ app.post('/api/mongo/login', function(req, res) {
   
         if (user) {
             const authToken = uuidv4();
-            res.send('Login successful');
             res.cookie('authToken', authToken, { 
-                maxAge: 60 * 1000, 
-                httpOnly: true
+              maxAge: 60000, 
+              httpOnly: true
             });
+            res.send(`
+              <p>Login successful<p>
+              ${viewCookies}
+              ${clearCookies}
+            `);
         } else {
             res.send(`
           <p>Invalid username or password<p>
@@ -126,5 +134,19 @@ app.post('/api/mongo/login', function(req, res) {
   
     run().catch(console.dir);
   });
+
+app.get('/api/mongo/view-cookies', function(req, res) {
+  res.send(`Active Cookies: ${JSON.stringify(req.cookies)}`);
+});  
+
+app.get('/api/mongo/clear-cookies', function(req, res) {
+  res.clearCookie('authToken');
+  res.send(`
+      <p>Clearing cookies...</p>
+      <p>Done.</p>
+      <p><a href="/api/mongo/view-cookies">View Active Cookies</a></p>
+      <p>Return to  <a href="/">login</a></p>
+  `);
+});
 
   
